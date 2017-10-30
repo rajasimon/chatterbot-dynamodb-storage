f981fdf54dea0363c09dcf29f9e8ebbbfbd05ade
Raja Simon <rajasimon@icloud.com>
2017-10-30 18:07:42 +0530

    Empty class function of DyanmoDB storage adpter

---
+++ chatterbot_dynamodb_storage/storage.py
@@ -0,0 +1,9 @@
+from chatterbot.storage import StorageAdapter
+
+
+class DynamodbStorageAdapter(StorageAdapter):
+    """
+    The DynamoDBStorageAdapter is an interface that allows
+    ChatterBot to store statements in a MongoDB database.
+    """
+    pass
